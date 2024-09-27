cbfd65c55ccc0204568bf03284553e7863c36a3c
hjasm1004 <hjasm1004@naver.com>
2024-09-27 15:15:14 +0900

    v0.2 IO,math,string

---
+++ week05/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt" // C언어의 #include <studio.h> 비슷한 역할을 한다
+
+func main() {
+	//var i int = 55
+	var f float32 = 3.99
+
+	//var i int
+	//i = 55
+
+	i := 55
+
+	fmt.Println("i는", i)
+	fmt.Println("kim inha")
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+}
